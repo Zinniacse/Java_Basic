94f2529848efc7e1f7127d313c8095b80062d2ae
Zinniacse <zinnia.cse7862@gmail.com>
2022-06-05 14:59:35 +0600

    inheritance

---
+++ ChildInheritance.java
@@ -0,0 +1,15 @@
+package Java_Basic;
+
+public class ChildInheritance {
+
+public static void main(String[] args) {
+		
+		HelloParent();	
+	
+		System.out.println(number);
+		add(10,30);
+		add(20,50);
+		add(10,100);
+	}
+
+}
---
+++ ParentInheritance.java
@@ -0,0 +1,23 @@
+package Java_Basic;
+
+public class ParentInheritance {
+
+	static void add (int num1,int num2) {
+		int sum = num1+num2;
+		
+		System.out.println("Sum " +sum);
+
+		}
+		//method create
+		static void HelloParent(){
+			System.out.println("its a parent Class"); 
+			
+		}
+		
+		static int number = 50;
+		
+		public static void main(String[] args) {
+
+	}
+
+}
