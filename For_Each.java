575b36412f7546178c7c21595f83b5fec81b0e55
Zinniacse <zinnia.cse7862@gmail.com>
2022-06-05 14:55:57 +0600

    for each loop

---
+++ For_Each.java
@@ -0,0 +1,35 @@
+package Java_Basic;
+import java.util.ArrayList;
+
+public class For_Each {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	       ArrayList<String> names = new ArrayList<String>();
+	       names.add("Name1");
+	       names.add("Name2");
+	       names.add("Name3");
+	       
+	        System.out.println(names.size());
+	        
+//	       names.remove(1);
+//	       System.out.println(names);
+//	       System.out.println(names.get(2));
+//	       
+//	       for (int i =0; i<names.size(); i++) {
+//	       System.out.println(names.get(i));
+//	       }
+	        
+	        //
+	       // for each loop: for(datatype item : array) 
+	       for (String value : names) {
+	    	   
+	    	   System.out.println(value);
+	    	   
+
+	}
+	}
+
+
+}
