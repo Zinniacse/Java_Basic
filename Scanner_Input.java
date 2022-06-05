63447ada70fcd3b90a365819b7eac6a8983aa647
Zinniacse <zinnia.cse7862@gmail.com>
2022-06-05 14:44:10 +0600

    input from user

---
+++ Scanner_Input.java
@@ -0,0 +1,29 @@
+package Java_Basic;
+
+public class Scanner_Input {
+
+	public static void main(String[] args) {
+		// INPUT TAKEN FROM USER
+		
+				Scanner input = new Scanner(System.in);
+				//data type variable name = value
+				
+				 System.out.print("Enter a Number: ");
+				 
+				 int number = input.nextInt();
+				 
+				 System.out.print("Enter a Name: ");
+				 
+				 String name = input.next();
+				 
+				 System.out.println("Number :" +number);
+				 
+				 System.out.println("Name :" +name);
+				 
+				 System.out.println("Name :" +name + " Number :" +number);
+
+	}
+
+	
+
+}
