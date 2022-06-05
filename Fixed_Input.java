c585e0abb4b9abc03ffb1be6d48f1dda1481eec9
Zinniacse <zinnia.cse7862@gmail.com>
2022-06-05 14:42:39 +0600

    variable Declaration

---
+++ Fixed_Input.java
@@ -0,0 +1,16 @@
+package Java_Basic;
+
+public class Fixed_Input {
+
+	public static void main(String[] args) {
+		// data type  variable name = value (variable declaration)
+		
+		int number = 10;
+	       String name = "Java";
+	       
+	       System.out.println("Number :" +number);
+	       System.out.println("Name :" +name);
+	       
+	}
+
+}
