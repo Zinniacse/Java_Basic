4f2980044bf42f69ad42403a06e58f9499556e36
Zinniacse <zinnia.cse7862@gmail.com>
2022-06-05 14:50:29 +0600

    for loop

---
+++ Loop.java
@@ -0,0 +1,19 @@
+package Java_Basic;
+
+import java.util.Scanner;
+
+public class Loop {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a Number : ");
+		
+		int number = input.nextInt();
+		
+		for (int i=0; i<=number; i++) {
+			
+			System.out.println("Number : "+i);
+		}
+		
+	}
+	}
